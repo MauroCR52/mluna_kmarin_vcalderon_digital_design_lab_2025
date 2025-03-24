6262aa38844c761e719747535115a5a8aaa2606c
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2025-03-24 09:29:26 -0600

    Update Subtractor.sv

--- laboratorio_2/Subtractor.sv
+++ laboratorio_2/Subtractor.sv
@@ -1,22 +1,33 @@
 module Subtractor #(parameter N = 4)(
     input logic [N-1:0] minuendo,
     input logic [N-1:0] sustraendo,
-    output logic [N-1:0] diferencia
+    output logic [N-1:0] diferencia,
+    output logic cout   
 );
-    // se transforma el sustraendo a un número negativo
 
-    logic[N:0] complemento_dos; 
-    logic[N:0] temp_sum;
-    logic cout;
-    // complemento a dos; transformar un número de N bits, en su complemento, y sumarle número, dando como resultado, el número negativo.
+    // Se transforma el sustraendo a un número negativo 
+    logic [N:0] complemento_dos; 
+    logic [N:0] temp_sum;
+    logic cout_internal;
+    
+    // Complemento a dos: invertir los bits y sumar 1
     assign complemento_dos = ~sustraendo + 1;
 
-    // Suma el minuendo (número al que se le va a restar), con el complemento, dando el resultado de la resta.
+    // Suma el minuendo con el complemento del sustraendo
     Adder #(N) restAdder (
         .num1(minuendo),
         .num2(complemento_dos),
         .sum(temp_sum),
-        .cout(cout)
+        .cout(cout_internal)
     );
-	 assign diferencia=(minuendo>sustraendo)?temp_sum[N-1:0]:(~temp_sum[N-1:0]+1);
+    
+
+    assign diferencia = temp_sum[N-1:0]; // No necesitamos el ~temp_sum, ya que la suma es directa.
+
+    
+    assign cout = cout_internal;
+
+
+    assign overflow = (minuendo[N-1] == sustraendo[N-1]) && (diferencia[N-1] != minuendo[N-1]);
+
 endmodule
